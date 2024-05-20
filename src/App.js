3d2086dd39d4d1834a54079d2ad894ad5fab256f
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-20 18:05:09 +0600

    Added the navbar, jumbotron, statistical cards, achievement timeline and footer for the homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import Navbar from './Components/Navbar/Navbar';
+import Footer from './Components/Footer/Footer';
+import Jumbotron from './Components/Jumbotron/Jumbotron';
+import HomeStatistics from './Components/Statistics/Statistics';
+import Timeline from './Components/Timeline/Timeline';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <Jumbotron/>
+      <HomeStatistics/>
+      <Timeline/>
+      <Footer/>
     </div>
   );
 }
