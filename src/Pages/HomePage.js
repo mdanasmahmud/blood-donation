8b5ad2cbf14b5d997e97bd5384896bc435af2dee
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-21 19:56:59 +0600

    Edited homepage to show dynamic data and only show news upto 3 news

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -4,12 +4,13 @@ import Jumbotron from '../Components/HomePage/Jumbotron/JumbotronComponent';
 import HomeStatistics from '../Components/HomePage/Statistics/StatisticsComponent';
 import Timeline from '../Components/HomePage/Timeline/TimelineComponent';
 
-function Home() {
+function Home(props) {
   return (
     <div>
       <Jumbotron/>
       <HomeStatistics/>
-      <Timeline/>
+      {console.log(props.newsList)}
+      <Timeline newsList={props.newsList}/>
     </div>
   );
 }
