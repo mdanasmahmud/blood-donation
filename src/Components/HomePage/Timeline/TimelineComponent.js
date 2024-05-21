562d2e66beed2f40059be7ba4eb74cad78e2097f
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-21 19:58:31 +0600

    Added the timeline component to show all the news in the news page

--- src/Components/HomePage/Timeline/TimelineComponent.js
+++ src/Components/HomePage/Timeline/TimelineComponent.js
@@ -1,24 +1,25 @@
 import React from "react";
+import TimelineDetails from "./TimelineDetailsComponent";
 
-const Timeline = () => {
+import  {NavLink} from 'react-router-dom'
+
+const Timeline = (props) => {
     return(
-
-<div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
-    <ol class="relative border-s border-gray-200 dark:border-gray-700">                  
-        <li class="mb-10 ms-4">
-            <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
-            <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">May 2024</time>
-            <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Created The Website</h3>
-            <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">This Website will act to help people who are in need of blood</p>
-            <a href="#" class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700">Learn more <svg class="w-3 h-3 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-    </svg></a>
-        </li>
-    </ol>
-
-</div>
-
-
+        <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16">
+            <ol class="relative border-s border-gray-200 dark:border-gray-700">                  
+                {props.newsList.map((singleNews) => (
+                    <>
+                    <li class="mb-10 ms-4">
+                        <div class="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
+                        <time class="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{singleNews.newsDate}</time>
+                        <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{singleNews.newsTitle}</h3>
+                        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{singleNews.shortDescription}</p>
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><NavLink to={`/news/${singleNews.news_id}`}>Learn More</NavLink></button>
+                    </li>
+                    </>
+                ))}
+            </ol>
+        </div>
     );
 }
 
