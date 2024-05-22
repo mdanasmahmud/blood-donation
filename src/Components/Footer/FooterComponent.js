8112c1bfe5547a8ec49ae6af2275c46d8304757e
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-23 01:11:41 +0600

    Fixed a bug where footer had a huge amount of padding

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -2,8 +2,6 @@ import React from "react";
 
 const Footer = () => {
     return(
-        <div class="flex flex-col min-h-screen">
-            <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4 mt-auto">
                 <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div class="sm:flex sm:items-center sm:justify-between">
                         <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
@@ -28,8 +26,6 @@ const Footer = () => {
                     <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
                     <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" class="hover:underline">Anas</a>. All Rights Reserved.</span>
                 </div>
-            </footer>
-        </div>
     );
 }
 
