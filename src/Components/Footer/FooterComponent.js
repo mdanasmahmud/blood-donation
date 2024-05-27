ea9de8316b659776710120892c82046265e07171
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-28 01:06:39 +0600

    fixed a small issue of logo not being shown

--- src/Components/Footer/FooterComponent.js
+++ src/Components/Footer/FooterComponent.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import bloodLogo from '../../images/blood-logo.png';
+
 const Footer = () => {
     return(
                 <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
                     <div class="sm:flex sm:items-center sm:justify-between">
                         <a href="" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                            <img src="" class="h-8" alt="Logo" />
+                            <img src={bloodLogo} class="h-8" alt="Logo" />
                             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Blood Donation</span>
                         </a>
                         <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
--- src/Components/Navbar/NavbarComponent.js
+++ src/Components/Navbar/NavbarComponent.js
@@ -3,6 +3,8 @@ import { NavLink } from 'react-router-dom';
 
 import NavbarUserComponent from './NavbarUserComponent';
 
+import bloodLogo from '../../images/blood-logo.png';
+
 function Navbar(props) {
   return (
 
@@ -10,7 +12,7 @@ function Navbar(props) {
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-      
+      <img src={bloodLogo} class="h-8" alt="Blood Logo"/>
       <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"><NavLink to='/'>Blood Donation</ NavLink></span>
   </a>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
