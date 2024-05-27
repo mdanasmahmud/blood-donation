39850aafcbfa05dd1e7649b98df7baff0508f0ed
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-27 20:27:38 +0600

    Added temporary userDashboard

--- src/Pages/UserDashboardPage.js
+++ src/Pages/UserDashboardPage.js
@@ -3,12 +3,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import UserSidebarComponent from "../Components/User/UserSidebarComponent";
 import UserProfileComponent from "../Components/User/UserProfileComponent";
+import UserDashboardComponent from "../Components/User/UserDashboardComponent";
 
 const UserDashboardPage = () => {
     return(
         <div>
             <UserSidebarComponent/>
             <Routes>
+                <Route path="" element={<UserDashboardComponent/>}/>
                 <Route path="profile" element={<UserProfileComponent/>}/>
             </Routes>
         </div>
