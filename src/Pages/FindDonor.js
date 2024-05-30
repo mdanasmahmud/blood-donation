5deb6f22a2bc07e7200846e1f23ee29ad1ad94cb
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-05-30 22:35:47 +0600

    Added the map component

--- src/Pages/FindDonor.js
+++ src/Pages/FindDonor.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import DonorList from '../Components/FindDonor/DonorList/DonorListComponent';
+import MapComponent from '../Components/FindDonor/MapComponent';
 
 const FindDonor = (props) => {
   return (
-    <DonorList donorList={props.donorList}/>
+    <div className='max-w-screen-xl flex flex-wrap justify-around items-center mx-auto p-4'>
+      <MapComponent />
+      <DonorList donorList={props.donorList}/>
+    </div>
   );
 }
 
