d1bcd51ff6f922fecdffa707aeaf2b9fc7054d73
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-01 18:23:19 +0600

    Added the routing of other routes in the app.js

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const bloodDonorRoutes = require('./routes/blood-doner-routes')
+const newsRoute = require('./routes/news-routes')
+const patientRoute = require('./routes/patient-routes')
+const appointmentRoute = require('./routes/appointment-routes')
+const userRoute = require('./routes/users-routes')
 
 const app = express();
 
-
-
-app.use('/api/blood-donors',bloodDonorRoutes);
+app.use('/api/appointments', appointmentRoute) // This will only take the userId and show the appointments of that user
+app.use('/api/patients', patientRoute) // This can show all patients Id and also 1 if needed
+app.use('/api/news', newsRoute) // Can show all patient Id or 1 if needed
+app.use('/api/blood-donors',bloodDonorRoutes); // Can show all blood donors or only 1 if needed
+app.use('/api/users', userRoute) // Only shows 1 user if needed
 
 app.listen(5000);
 
