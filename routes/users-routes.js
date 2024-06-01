6e3706e86a16e663c0eaf1d486653b7cc4a78411
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-01 18:24:50 +0600

    Edited the users-routes to only give one user's info, mostly for signing in

---
+++ routes/users-routes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const router = express.Router();
+
+const users = [
+    {user_id: 0, userName: 'Anas Mahmud', password:'1234', email:'abid1234@gmail.com'},
+    {user_id: 1, userName: 'John Doe', password:'4321', email:'johndoe1234@gmail.com'}
+ ]
+
+router.get('/:user_id', (req, res, next) => {
+    const userId = Number(req.params.user_id);
+    const userOneId = users.find(p => {
+        return p.user_id === userId;
+    })
+    console.log('user requested get');
+    res.json({userOneId});
+});
+
+
+module.exports = router;
