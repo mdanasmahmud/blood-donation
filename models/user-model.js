a78b994fbd6fcb138c2cdc686f6a288f3ffcaea8
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-03 20:32:20 +0600

    Added minlength and unique true to email so we can find email faster

--- models/user-model.js
+++ models/user-model.js
@@ -1,11 +1,15 @@
+const uniqueValidator = require('mongoose-unique-validator')
+
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
 
 const userSchema = new Schema({
     userName: {type: String, required: true},
-    password: {type: String, required: true},
-    email: {type: String, required: true}
+    password: {type: String, required: true, minlength: 6},
+    email: {type: String, required: true, unique: true}
 })
+
+userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema)
