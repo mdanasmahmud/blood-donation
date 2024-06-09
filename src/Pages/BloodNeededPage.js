4d1b9799c24e592ac940274611ebddd2d2665180
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-09 15:57:13 +0600

    Added dynamic data retrival from the backend

--- src/Pages/BloodNeededPage.js
+++ src/Pages/BloodNeededPage.js
@@ -1,42 +1,39 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
 
 import BloodNeededListComponent from '../Components/BloodNeeded/BloodNeededListComponent';
 import BloodNeededSearchComponent from '../Components/BloodNeeded/BloodNeededSearchComponent';
 import BloodNeededDetailsComponent from '../Components/BloodNeeded/BloodNeededDetailsComponent';
 import BloodNeededPost from '../Components/BloodNeeded/BloodNeededPost';
 
-const BloodNeededPage = (props) => {
+const BloodNeededPage = () => {
   
-  const [searchedText, setSearchedText] = useState('')
+  const [searchedText, setSearchedText] = useState('');
+  const [patientDetails, setPatientDetails] = useState([]);
+  const [clickedPatientId, setClickedPatientId] = useState('');
 
-  // This is for search function
+  useEffect(() => {
+  fetch('http://localhost:5000/api/patients/')
+    .then(response => response.json())
+    .then(data => setPatientDetails(data.patients)) // set the state to the array of patients
+    .catch(error => console.error('Error:', error));
+}, []);
 
-  const patientDetail = props.patientDetails.filter(patient => 
-    patient.patientLocation.includes(searchedText) || 
-    patient.patientBloodGroup.includes(searchedText)
-  )
+const patientDetail = patientDetails.filter(patient => 
+  patient.patientLocation.includes(searchedText) || 
+  patient.patientBloodGroup.includes(searchedText)
+);
 
-  // This is for model details function
-
-  const [clickedPatientId, setClickedPatientId] = useState('')
-
-  const patientDeeperDetails = props.patientDetails.find(patientDeeper =>
-    patientDeeper.patient_id === clickedPatientId
-  )
+const patientDeeperDetails = patientDetails.find(patientDeeper =>
+  patientDeeper._id === clickedPatientId // use _id as it seems to be the unique identifier
+);
 
   return (
     <div>
-      
-      <BloodNeededSearchComponent searchedText={searchedText} setSearchedText={setSearchedText} patientDetails={props.patientDetails}/>
-
+      <BloodNeededSearchComponent searchedText={searchedText} setSearchedText={setSearchedText} patientDetails={patientDetails}/>
       <div className='flex justify-center'>
         <BloodNeededPost />
         <BloodNeededListComponent patientDetails={patientDetail} clickedPatientId={clickedPatientId} setClickedPatientId={setClickedPatientId}/>
-        
       </div>
-      
-      
-      
       <BloodNeededDetailsComponent patientDeeperDetails = {patientDeeperDetails} setClickedPatientId={setClickedPatientId}/>
     </div>
   );
