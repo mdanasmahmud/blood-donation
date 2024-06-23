a3322da6ac8b5a6bb53f240962ee8e3d61e7cbb9
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-23 16:35:12 +0600

    Added the feature to update the time the user wants to visit the appointment and also the status if the user is not available anymore for the appointment

--- src/Components/User/UserDashboardAppointmentComponent.js
+++ src/Components/User/UserDashboardAppointmentComponent.js
@@ -4,6 +4,7 @@ import { AuthContext } from "../../context/auth-contex";
 const UserDashboardAppointmentComponent = () => {
   const auth = useContext(AuthContext);
   const [appointments, setAppointments] = useState([]);
+  const [updateTrigger, setUpdateTrigger] = useState(false);
 
   useEffect(() => {
     const fetchAppointments = async () => {
@@ -26,7 +27,56 @@ const UserDashboardAppointmentComponent = () => {
     };
 
     fetchAppointments();
-  }, [auth.userId]);
+  }, [auth.userId, updateTrigger]);
+
+  // This is for updating the appointments
+
+  const [isAppointmentModalOpen, setIsAppointmentModalOpen] = useState(false)
+
+  const [selectAppointmentId, setSelectAppointmentId] = useState('')
+
+  const [appointmentDateTime, setAppointmentDateTime] = useState('');
+  const [appointmentStatus, setAppointmentStatus] = useState('');
+
+  const appointmentModalOpen = (appointment) => {
+    setIsAppointmentModalOpen(true)
+    setSelectAppointmentId(appointment._id); // Set the selected appointment ID
+    setAppointmentDateTime(appointment.preferredTime); // Set the default appointment date and time
+    setAppointmentStatus(appointment.status);
+  }
+
+  const appointmentModalClose = () => {
+    setIsAppointmentModalOpen(false)
+  }
+
+  const updateAppointmentHandler = async () => {
+    console.log(appointmentStatus)
+    try {
+      const response = await fetch(`http://localhost:5000/api/appointments/updateAppointment`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + auth.token,
+        },
+        body: JSON.stringify({
+          appointmentId: selectAppointmentId,
+          preferredTime: appointmentDateTime,
+          status: appointmentStatus
+        })
+      })
+      const responseData = await response.json();
+      if (!response.ok) {
+        throw new Error(responseData.message)
+      }
+      console.log('Appointment Update Successfull')
+      setUpdateTrigger(prev => !prev);
+      appointmentModalClose()
+    }
+    catch (err){
+      console.log(err)
+    }
+  }
+  
 
   return (
     <>
@@ -49,13 +99,83 @@ const UserDashboardAppointmentComponent = () => {
               <button
                 type="button"
                 className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-4"
+                onClick={() => appointmentModalOpen(appointment)}
               >
-                Update Post
+                Update Appointment
               </button>
             </div>
           </div>
         ))}
       </div>
+      {/* This modal is to update the appointments */}
+      {isAppointmentModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+          <div className="absolute w-full h-full bg-black opacity-50"></div>
+          <div className="relative p-4 w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700 z-50">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+                Appointment Details
+              </h3>
+            </div>
+            <div className="p-4 md:p-5 space-y-4">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="datetime-local"
+                name="appointmentDateTime"
+                id="appointmentDateTime"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" "
+                value={appointmentDateTime}
+                onChange={(e) => setAppointmentDateTime(e.target.value)}
+                required
+              />
+              <label
+                htmlFor="appointmentDateTime"
+                className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+              >
+                Appointment Date & Time
+              </label>
+            </div>
+              <div className="relative z-0 w-full mb-5 group">
+                <select
+                  value={appointmentStatus}
+                  onChange={(e) => (setAppointmentStatus(e.target.value))}
+                  name="appointmentStatus"
+                  id="appointmentStatus"
+                  className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                  required
+                >
+                  <option value="" disabled selected>Select your option</option>
+                  <option value="Pending">Pending</option>
+                  <option value="Not Available">Not Available</option>
+                </select>
+                <label
+                  htmlFor="appointmentStatus"
+                  className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                >
+                  Status
+                </label>
+              </div>
+            </div>
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              <button
+                type="button"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                onClick={updateAppointmentHandler }
+              >
+                Update
+              </button>
+              <button
+                type="button"
+                className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                onClick={appointmentModalClose}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
