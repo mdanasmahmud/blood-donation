c9987f667e3388635261e9721757179a4bfeb58f
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-06-25 17:10:39 +0600

    Just added the feature that will show user details at the front page of user dashboard

---
+++ src/Components/User/UserDashboardUserDetailsComponent.js
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from "react";
+
+const UserDetailsComponent = ({userId, userToken}) => {
+
+    const [userName, setUserName] = useState('')
+    const [userEmail, setUserEmail] = useState('')
+
+    useEffect(() => {
+        const fetchUserDetails = async () => {
+            try {
+                const response = await fetch(`http://localhost:5000/api/users/${userId}`, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": "Bearer " + userToken,
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error("Fetching user data failed");
+                }
+
+                const data = await response.json();
+                const { user } = data;
+                setUserName(user.userName)
+                setUserEmail(user.email)
+
+            } catch (error) {
+                console.error("An error occurred while fetching user data:", error);
+            }
+        }
+        fetchUserDetails();
+    }, [userId])
+
+
+    return(
+        <div className="m-8 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+            <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">User Details</h5>
+            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Name: {userName}</p>
+            <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">Email: {userEmail}</p>
+        </div>
+    )
+}
+
+export default UserDetailsComponent
