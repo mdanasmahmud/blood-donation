8971ec825117f50db206d65fca1bf20a90943f0a
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-03 17:00:06 +0600

    Registered donors will not be able to register

--- src/Components/FindDonor/BecomeDonorComponent.js
+++ src/Components/FindDonor/BecomeDonorComponent.js
@@ -81,6 +81,35 @@ const BecomeDonorComponent = ({userId, userToken, setShowModal, userGeo}) => {
       submitBloodDonorInfo()
     }
 
+    // This is to do a fetch request to know if the user exists
+
+    const [checkBloodDonorExists, setcheckBloodDonorExists] = useState(false)
+
+    useEffect(() => {
+      const fetchUserDetails = async () => {
+        try{
+          const response = await fetch(`http://localhost:5000/api/blood-donors/getDonorById/${userId}`,{
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": 'Bearer ' + userToken,
+            }
+          })
+          if(!response.ok){
+            throw new Error("Failed to get the user details")
+          }
+          const data = await response.json()
+          setcheckBloodDonorExists(true);
+          
+        }
+        catch (error){
+          console.log("Catched Error: " + error)
+        }
+        
+      }
+      fetchUserDetails()
+  }, [userId])
+
 
     return(
     <>  
@@ -160,9 +189,12 @@ const BecomeDonorComponent = ({userId, userToken, setShowModal, userGeo}) => {
             </div>
 
             <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+              {checkBloodDonorExists ? 
+              <button type="button" class="py-2.5 px-5 text-sm text-white bg-blue-400 dark:bg-blue-500 cursor-not-allowed font-medium rounded-lg text-center" disabled>Already A Donor</button>
+              : 
               <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" onClick={submitBloodDonorHandler}>
-                Apply
-              </button>
+                  Apply
+              </button>}
               <button type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={() => setShowModal(false)}>
                 Cancel
               </button>
