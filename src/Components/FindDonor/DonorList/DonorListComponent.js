5834eb8cf247548bd95faf865562de6a28b7316c
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-05 05:49:27 +0600

    Just added a border to the list

--- src/Components/FindDonor/DonorList/DonorListComponent.js
+++ src/Components/FindDonor/DonorList/DonorListComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 const DonorList = ({donorList, setClickedId}) => {
     return(
         <div>
-            <div class="relative overflow-x-auto">
+            <div class="relative overflow-x-auto bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
@@ -35,7 +35,7 @@ const DonorList = ({donorList, setClickedId}) => {
                             <td class="px-6 py-4">
                                 {donor.donorPhone}
                             </td>
-                            <td class="px-6">
+                            <td class="px-6 py-3">
                             <button onClick={() => setClickedId(donor.location)} type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
