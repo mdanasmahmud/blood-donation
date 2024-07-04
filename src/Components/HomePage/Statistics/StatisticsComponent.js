c5f55e7e1ee10a32b6c852aeb0d0ed1fd85ac7f4
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-05 03:58:00 +0600

    Fixed a minor bug, replaced href with link from react router dom for seemless navigatian

--- src/Components/HomePage/Statistics/StatisticsComponent.js
+++ src/Components/HomePage/Statistics/StatisticsComponent.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
 
 const HomeStatistics = () => {
     const [bloodDonorList, setBloodDonorList] = useState([]);
@@ -41,20 +42,20 @@ const HomeStatistics = () => {
         <>
             <div class="flex items-center justify-center">
                 {/* Donor Card */}
-                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mr-4">
+                <Link to="/find-donor" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 mr-4">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Donors</h5> 
                     <div class="flex justify-center items-center h-full">
                         <span class="text-2xl text-gray-700 dark:text-gray-400 text-center">{bloodDonorList.length}</span>
                     </div>
-                </a>
+                </Link>
 
                 {/* Patient Card */}
-                <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                <Link to="/blood-needed" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Total Patients</h5>
                     <div class="flex justify-center items-center h-full">
                         <span class="text-2xl text-gray-700 dark:text-gray-400 text-center">{patientDetails.length}</span>
                     </div>
-                </a>
+                </Link>
             </div>
         </>
     );
