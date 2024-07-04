fa51f91ef6ab6864eb03f02f75aceb3b67ac5dc9
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-05 05:30:23 +0600

    Fixed a small alignment issue for the contact page and made both of the element next to each other

--- src/Components/About/UserContact.js
+++ src/Components/About/UserContact.js
@@ -19,7 +19,7 @@ const UserContact = (props) => {
     return (
         <div>
             {buttonClicked && <BannerComponent message = {`Dear ${name}, your message has been sent`}/>}
-            <form class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5 max-w-screen-sm mx-auto">
+            <form class=" bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 p-5 max-w-screen-sm">
             <div>
                     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Contact Us</h5>
                 </div>
@@ -32,8 +32,8 @@ const UserContact = (props) => {
                 <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="To Contact You Later" required value={email} onChange={(e) => setEmail(e.target.value)}/>
             </div>
                 <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                <button type="button" class="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={handleChange}>Send Message</button>
+                <textarea id="message" rows="4" class="block p-2 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..." value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                <button type="button" class="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-1.5" onClick={handleChange}>Send Message</button>
             </form>
         </div>
     )
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -11,13 +11,13 @@ const AboutPage = () => {
         <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">About</h1>
         <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Here are all the FAQ's and Contacts </p>
       </div>
-      <div className='flex justify-center space-x-5 mt-5 space-y-11'>
+      <div className='flex justify-center space-x-5 mt-5 space-y-5'>
         <AboutComponent />
-
-      </div>
-      <div className=' m-5'>
+       
         <UserContact />
+      
       </div>
+      
     </>
     
   );
