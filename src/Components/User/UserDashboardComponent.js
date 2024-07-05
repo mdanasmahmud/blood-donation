94cf5725cfb599b16aec0611616175b376de59c1
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-05 19:45:31 +0600

    Added user statistics component in the dashboard

--- src/Components/User/UserDashboardComponent.js
+++ src/Components/User/UserDashboardComponent.js
@@ -2,16 +2,18 @@ import React, {useContext } from "react";
 import { AuthContext } from "../../context/auth-contex";
 
 import UserDetailsComponent from "./UserDashboardUserDetailsComponent"
+import UserDashboardStatisticsComponent from "./UserDashboardStatisticsComponent"
 
 const UserDashboardComponent = () => {
 
   const auth = useContext(AuthContext)
 
   return (
-    <div>
-      <UserDetailsComponent userId={auth.userId} userToken = {auth.token}/>
+    <div className="flex flex-col justify-center items-center">
+      <UserDetailsComponent userId={auth.userId} userToken={auth.token}/>
+      <UserDashboardStatisticsComponent/>
     </div>
-  );
+);
 };
 
 export default UserDashboardComponent;
