68fc29b46399e495e81f580afd6cd171e56c5901
Md. Anas Mahmud <md.anas.mahmud@g.bracu.ac.bd>
2024-07-06 16:45:02 +0600

    Removed the static news data, news are now shown dynamically through the server

--- src/App.js
+++ src/App.js
@@ -21,20 +21,6 @@ import { useAuth } from './hooks/auth-hook';
 
 function App() {
 
-const newsList = [
-  {news_id: 0, newsTitle: 'Created Website', shortDescription: 'This Website will act to help people who are in need of blood', newsDate: '10-23-2021', newsDescription: 'This Website will act to help people who are in need of blood. This took me a lot of time to think of', newsAuthor: 'Anas'},
-  {news_id: 1, newsTitle: 'Will Deploy', shortDescription: 'Will Deploy this website', newsDate: '10-21-2021', newsDescription: 'Lot of things need to be made for this website to work.', newsAuthor: 'Anas'},
-  {news_id: 2, newsTitle: 'Website Launched', shortDescription: 'Our website is now live!', newsDate: '10-24-2021', newsDescription: 'Our website is now live! We are excited to start our journey.', newsAuthor: 'Anas'},
-  {news_id: 3, newsTitle: 'First Donation', shortDescription: 'We have received our first blood donation', newsDate: '10-25-2021', newsDescription: 'We have received our first blood donation. This is a big step for us.', newsAuthor: 'Anas'},
-  {news_id: 4, newsTitle: 'Partnership Announcement', shortDescription: 'We have partnered with a local hospital', newsDate: '10-26-2021', newsDescription: 'We have partnered with a local hospital to ensure the safety of our donors and recipients.', newsAuthor: 'Anas'},
-  {news_id: 5, newsTitle: 'New Features', shortDescription: 'New features have been added to the website', newsDate: '10-27-2021', newsDescription: 'New features have been added to the website to improve user experience.', newsAuthor: 'Anas'},
-  {news_id: 6, newsTitle: 'Success Story', shortDescription: 'Read about our latest success story', newsDate: '10-28-2021', newsDescription: 'Read about our latest success story. This is why we do what we do.', newsAuthor: 'Anas'},
-  {news_id: 7, newsTitle: 'Donor Appreciation Day', shortDescription: 'Celebrating our donors', newsDate: '10-29-2021', newsDescription: 'Celebrating our donors who make this all possible. Thank you for your generosity.', newsAuthor: 'Anas'},
-  {news_id: 8, newsTitle: 'Improved Search', shortDescription: 'We have improved our donor search feature', newsDate: '10-30-2021', newsDescription: 'We have improved our donor search feature to help you find a match faster.', newsAuthor: 'Anas'},
-  {news_id: 9, newsTitle: 'Mobile App Launch', shortDescription: 'Our mobile app is now available', newsDate: '10-31-2021', newsDescription: 'Our mobile app is now available. Download it today to stay updated.', newsAuthor: 'Anas'},
-  {news_id: 10, newsTitle: 'Year End Review', shortDescription: 'Looking back at our achievements this year', newsDate: '11-01-2021', newsDescription: 'Looking back at our achievements this year. We couldnt have done it without you.', newsAuthor: 'Anas'},
-];
-
   const {token, login, logout, userId} = useAuth()
 
   // To send a post request you need to add the token as the header
@@ -47,11 +33,11 @@ const newsList = [
   if (token) {
     routes = (
       <React.Fragment>
-        <Route path='/' element={<Home newsList={newsList.slice(0, 3)} totalDonors={0} />} />
+        <Route path='/' element={<Home/>} />
         <Route path='find-donor' element={<FindDonor />} />
         <Route path='blood-needed' element={<BloodNeededPage/>} />
-        <Route path='news/' element={<TimelineList newsList={newsList} />} />
-        <Route path='news/:news_id/' element={<TimelineDetails newsList={newsList} />} />
+        <Route path='news/' element={<TimelineList />} />
+        <Route path='news/:news_id/' element={<TimelineDetails />} />
         <Route path='about/' element={<AboutPage />} />
         <Route path='user/dashboard/*' element={<UserDashboardPage/>} />
         <Route path='*' element={<Navigate to='/' />} />
@@ -60,11 +46,11 @@ const newsList = [
   } else {
     routes = (
       <React.Fragment>
-        <Route path='/' element={<Home newsList={newsList.slice(0, 3)} totalDonors={0} />} />
+        <Route path='/' element={<Home/>} />
         <Route path='find-donor' element={<FindDonor />} />
         <Route path='blood-needed' element={<BloodNeededPage />} />
-        <Route path='news/' element={<TimelineList newsList={newsList} />} />
-        <Route path='news/:news_id/' element={<TimelineDetails newsList={newsList} />} />
+        <Route path='news/' element={<TimelineList />} />
+        <Route path='news/:news_id/' element={<TimelineDetails/>} />
         <Route path='about/' element={<AboutPage />} />
         <Route path='sign-up/' element={<SignUpComponent />} />
         <Route path='forgot-password/' element={<ForgotPasswordComponent />} />
